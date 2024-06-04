5aaf0cb26c6a65282b767c4b39b94802ca2ec712
Jettechnologies <tomesitime04@gmail.com>
2024-06-04 17:25:30 +0000

    Completed the shop page

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,36 +3,39 @@ import { StarIcon } from "../icons/svg";
 import { Heart } from "lucide-react";
 
 interface Props{
-    tag: string;
+    tag?: string;
 }
 
 const ProductCard:React.FC<Props> = ({tag}) => {
   return (
-    <div className="w-full h-full flex flex-col relative">
-        <div className="w-full flex justify-center items-center h-[75%] rounded-md bg-gray product-img">
-            <div className="flex justify-between w-full pt-3 px-3 absolute top-0 left-0">
-                <div className="p-2 rounded-sm text-base uppercase font-semibold bg-white">
-                    <p>{tag}</p>
+    <div className="w-full h-full shadow-sm rounded-md">
+        <div className="w-full h-full relative z-20">
+            <div className="w-full flex justify-center items-center h-[75%] rounded-md bg-gray product-img">
+                <div className="flex justify-between w-full pt-3 px-3 absolute top-0 left-0 z-20">
+                        {tag && <div className="p-2 rounded-sm text-base uppercase font-semibold bg-white">
+                            <p>{tag}</p>
+                    </div>}
+                    <div className="w-8 h-8 flex items-center justify-center rounded-full bg-white shadow-sm wishlist-icon cursor-pointer">
+                        <Heart size = {20}/>
+                    </div>
                 </div>
-                <div className="w-8 h-8 flex items-center justify-center rounded-full bg-white shadow-sm wishlist-icon cursor-pointer">
-                    <Heart size = {20}/>
-                </div>
+                <img src={headphoneImg} alt="product image" className="w-[60%] aspect-square object-contain"/>
             </div>
-            <img src={headphoneImg} alt="product image" className="w-[80%] aspect-square object-cover"/>
-        </div>
-        <div className="w-full h-[25%] pt-4 flex flex-col gap-y-2">
-            <div className="w-fit flex gap-1">
+            <div className="w-full h-[25%] pt-4 flex flex-col gap-y-2">
+                <div className="w-fit flex gap-1">
                 {
                     [1,2,3,4,5].map((index) => (
                         <StarIcon className="text-yellow" fill="yellow" key={index}/>
                     ))
                 }
+                </div>
+                <p className="text-size-500 font-medium">Lorem ipsum dolor sit.</p>
+                <p className="text-size-500 font-medium">$100</p>
             </div>
-            <p className="text-size-500 font-medium">Lorem ipsum dolor sit.</p>
-            <p className="text-size-500 font-medium">$100</p>
         </div>
     </div>
   )
 }
 
 export default ProductCard
+
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,5 @@
 // import ProductList from "../components/ProductList";
-import Filters from "../components/Filters";
+// import Filters from "../components/Filters";
 import Modal from "../components/Modal";
 // import { useParams } from "react-router";
 // import { useNavigate } from "react-router-dom";
@@ -9,8 +9,9 @@ import { Link, useLocation } from "react-router-dom";
 import { DropdownIcon } from "../icons/svg";
 import Select from "../components/Select";
 import SideNavBar from "../components/SideNavBar";
-import { CircleX, DollarSign } from "lucide-react";
+import { CircleX, DollarSign, SlidersHorizontal } from "lucide-react";
 import ReactSlider from "react-slider";
+import ProductCard from "../components/ProductCard";
 
 function Store() {
   // const { search, category = "all" } = useParams<{
@@ -43,7 +44,7 @@ function Store() {
   return (
     <>
       <section className="w-full h-full px-8">
-        <div id = "page-banner" className="w-full h-[300px] bg-gray flex flex-col justify-center items-center gap-y-4 border-2 border-black">
+        <div id = "page-banner" className="w-full h-[300px] bg-gray flex flex-col justify-center items-center gap-y-4">
           <ul className="w-fit flex gap-2 text-size-400 font-semibold text-text-black capitalize">
             <li className="flex items-center gap-2">
               <Link to = "/">
@@ -70,92 +71,39 @@ function Store() {
           </p>
         </div>
 
-        <div className="border-b border-black flex justify-end py-8">
+        <div className="border-b border-black py-8">
+          <div className="flex justify-between w-full items-center">
+              <button 
+                className="p-2 flex gap-4 items-center hover:underline" 
+                type="button"
+                onClick = {handleClick}
+              >
+                <SlidersHorizontal />
+                <p className="text-text-black font-medium text-size-500">Filters</p>
+              </button>
             <div className="flex gap-x-4 items-center">
               <label htmlFor="sort">
                 <p className="text-size-500 font-medium text-text-black">Sort by</p>
               </label>
-              <Select className="border border-gray " select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
+              <Select className="border-2 border-gray " select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
             </div>
+          </div>
         </div>
         {/* the main section */}
-        <div id = "main-section" className="border-2 border-black">
-            <div className="flex">
-              <div className="w-[40vw]">
-                <SideNavBar>
-                  <div className="border-b border-black pb-4">
-                    <h3 className="font-semibold text-xl capitalize text-text-black mb-4">filters</h3>
-                    <ul className="flex flex-wrap gap-3">
-                      <li className="p-3 bg-blue text-white flex gap-2 rounded-md">
-                        <p className="font-medium text-sm">
-                          Lorem ipsum
-                        </p>
-                        <CircleX size = {20} color = "#990000"/>
-                      </li>
-                      <li className="p-3 bg-blue text-white flex gap-2 rounded-md">
-                        <p className="font-medium text-sm">
-                          Lorem ipsum
-                        </p>
-                        <CircleX size = {20} color = "#990000"/>
-                      </li>
-                      <li className="p-3 bg-blue text-white flex gap-2 rounded-md">
-                        <p className="font-medium text-sm">
-                          Lorem ipsum
-                        </p>
-                        <CircleX size = {20} color = "#990000"/>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="border-b border-black p-4 flex flex-col gap-4">
-                    <p className="font-medium text-size-500 text-text-black capitalize">
-                      categories
-                    </p>
-                    <div className="w-full h-full">
-                      <Select className="border border-black" select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
+        <div id = "main-section" className="mt-8">
+
+            {/* exte */}
+
+            <div className="flex border-2 border-black relative">
+              <div className="flex flex-wrap justify-between gap-4">
+                      {
+                        [1,2,3,4,5,6,7,8].map(index => (
+                          <div className="w-full md:w-[44.5vw] lg:w-[22.5vw] h-[23rem] z-20" key={index}>
+                            <ProductCard />
+                          </div>
+                        ))
+                      }
                     </div>
-                  </div>
-                  <div className="border-b border-black p-4 flex flex-col gap-4">
-                    <p className="font-medium text-size-500 text-text-black capitalize">
-                      sort by
-                    </p>
-                    <div className="w-full h-full">
-                      <Select className="border border-black" select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
-                    </div>
-                  </div>
-                  <div className="border-b border-black p-4 flex flex-col gap-4">
-                    <p className="font-medium text-size-500 text-text-black capitalize">
-                      price
-                    </p>
-                    <div className="flex flex-col gap-y-6">
-                      <div className="flex justify-between">
-                        <div className="border border-black px-3 py-4 w-[8rem] flex items-center">
-                          <DollarSign size={15}/> {values[0]}
-                        </div>
-                        <div className="border border-black px-3 py-4 w-[8rem] flex items-center">
-                          <DollarSign size={15}/> {values[1]}
-                        </div>
-                      </div>
-                      <ReactSlider
-                          className="horizontal-slider"
-                          thumbClassName="price-thumb"
-                          trackClassName="price-track"
-                          defaultValue={values}
-                          ariaLabel={['Lower thumb', 'Upper thumb']}
-                          ariaValuetext={state => `Thumb value ${state.valueNow}`}
-                          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-                          pearling
-                          onChange={(value) => setValues(value)}
-                          minDistance={10}
-                          min = {MIN}
-                          max = {MAX}
-                      />
-                    </div>
-                  </div>
-                </SideNavBar>
-              </div>
-              <div className="flex-1 border-2 border-red-500 min-h-screen">
-                
-              </div>
             </div>
         </div>
 
@@ -163,7 +111,54 @@ function Store() {
 
       <Modal position="left" open={isFiltersOpen} onClose={handleClick}>
         <div onClick={handleClick}>
-          <Filters />
+        <div className="border-b border-black pb-4">
+                      <h3 className="font-semibold text-xl capitalize text-text-black mb-4">filters</h3>
+                      <ul className="flex flex-wrap gap-3">
+                      </ul>
+                    </div>
+                    <div className="border-b border-black p-4 flex flex-col gap-4">
+                      <p className="font-medium text-size-500 text-text-black capitalize">
+                        categories
+                      </p>
+                      <div className="w-full h-full">
+                        <Select className="border border-black" select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
+                      </div>
+                    </div>
+                    <div className="border-b border-black p-4 flex flex-col gap-4">
+                      <p className="font-medium text-size-500 text-text-black capitalize">
+                        sort by
+                      </p>
+                      <div className="w-full h-full">
+                        <Select className="border border-black" select={[{key: "recommend", value: "recommend"}, {key: "customer review", value: "customer review"}, {key: "lowest - highest", value: "lowest - highest"}]}/>
+                      </div>
+                    </div>
+                    <div className="border-b border-black p-4 pb-6 flex flex-col gap-4">
+                      <p className="font-medium text-size-500 text-text-black capitalize">
+                        price
+                      </p>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex justify-between">
+                          <div className="border border-black px-3 py-4 w-[8rem] flex items-center">
+                            <DollarSign size={15}/> {values[0]}
+                          </div>
+                          <div className="border border-black px-3 py-4 w-[8rem] flex items-center">
+                            <DollarSign size={15}/> {values[1]}
+                          </div>
+                        </div>
+                        <ReactSlider
+                            className="horizontal-slider"
+                            thumbClassName="price-thumb"
+                            trackClassName="price-track"
+                            defaultValue={values}
+                            ariaLabel={['Lower thumb', 'Upper thumb']}
+                            pearling
+                            onChange={(value) => setValues(value)}
+                            minDistance={10}
+                            min = {MIN}
+                            max = {MAX}
+                        />
+                      </div>
+                    </div>
         </div>
       </Modal>
     </>
