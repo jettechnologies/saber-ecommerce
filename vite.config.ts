d4e02647908b11029e59d9955f3f3e3625241733
Jettechnologies <tomesitime04@gmail.com>
2024-08-08 03:29:36 +0000

    fixing babel runtime error during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import reactSlider from 'react-slider';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
   resolve: {
     alias: {
     "@": path.resolve(__dirname, "./src"),
+  },
+  build: {
+    rollupOptions: {
+      external: ['@babel/runtime', reactSlider],
+    },
   },
 },
 })
